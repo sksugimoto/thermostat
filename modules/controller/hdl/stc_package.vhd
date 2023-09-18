f1b64491a73dd253032d7cbe207713bd4bfe505c
sksugimoto <sksugimoto@ucdavis.edu>
2023-09-17 17:24:32 -0700

    Added offset c-to-f and f-to-c functions.

--- modules/controller/hdl/stc_package.vhd
+++ modules/controller/hdl/stc_package.vhd
@@ -8,6 +8,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 -- use ieee.std_logic_unsigned.all;
 use ieee.numeric_std.all;
+use ieee.fixed_pkg.all;
 
 package stc_package is
   -- Standard Thermostat Configuration (STC) Record Definition
@@ -34,9 +35,14 @@ package stc_package is
     i_slv_stc : in  std_logic_vector(31 downto 0)
   ) return t_stc;
 
-end package stc_package;
+  function stc_offset_c_to_f (
+    i_c_offset : in integer
+  ) return integer;
 
-package body stc_package is
+  function stc_offset_f_to_c (
+    i_f_offset : in integer
+  ) return integer;
+
   constant c_stc_idle : t_stc := (
     heat_on     => '0',
     cool_on     => '0',
@@ -44,6 +50,9 @@ package body stc_package is
     trgt_c_ofst => 0,
     trgt_f_ofst => 0
   );
+end package stc_package;
+
+package body stc_package is
   -- Converts stc to a 32-bit wide slv.
   function stc_to_slv (
     i_stc_t : in t_stc
@@ -74,4 +83,33 @@ package body stc_package is
     v_stc.trgt_f_ofst := to_integer(unsigned(i_slv_stc(5 downto 0)));
     return v_stc;
   end ;
+
+  -- Converts Celsius offset to Fahrenheit offset (F = 1.8*C + 32)
+  function stc_offset_c_to_f (
+    i_c_offset : in integer
+  ) return integer is
+    constant c_f_mult   : ufixed(0 downto -9) := 10x"399";
+    -- constant c_f_offset : ufixed(5 downto 0)  := 6x"20";
+    -- variable v_c_temp   : ufixed(5 downto -1);
+    variable v_f_offset : integer;
+  begin
+  
+    -- v_c_temp    := resize((to_ufixed(i_c_offset, 5, 0) * to_ufixed(0.5, 0, -1)) + to_ufixed(10, 3, 0), v_c_temp'high, v_c_temp'low);
+    -- v_f_offset  := 99 - to_integer(resize(v_c_temp * c_f_mult + c_f_offset, 5, -1));
+    v_f_offset  := to_integer((to_ufixed(i_c_offset, 5, 0) * to_ufixed(0.5, 0, -1)) * c_f_mult);
+    return v_f_offset;
+  end;
+
+  function stc_offset_f_to_c (
+    i_f_offset : in integer
+  ) return integer is
+    -- 1/1.8 = 0.555555556
+    -- 100|0111 = 0.5+0.3125+0.015625+0.0078128 = 0.5546875
+    constant c_c_mult : ufixed(-1 downto -7) := 7x"47";
+    variable v_c_offset : integer;
+  begin
+    v_c_offset := to_integer((to_ufixed(i_f_offset, 5, 0) * c_c_mult) * to_ufixed(2, 1, 0));
+    return v_c_offset;
+  end;
+
 end package body stc_package;
